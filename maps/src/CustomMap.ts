8ed1c2b188b6600b927b9e754fbe3b225b2ebdbc
Oliver-Strange <preston.burton@gmail.com>
2020-05-28 11:03:15 -0700

    adds google map and markers

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,65 @@
+/// <reference types="@types/googlemaps" />
+import { User } from './User';
+import { Company } from './Company';
+
+// Instructions to every other class on how
+// they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  // addMarker can now create a maker for anything that uses the interface
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+
+  // Bad code in that it is duplicated with add company marker, not DRY
+  // addUserMaker(user: User): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: user.location.lat,
+  //       lng: user.location.lng,
+  //     },
+  //   });
+  // }
+
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng,
+  //     },
+  //   });
+  // }
+}
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,11 +1,16 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class User {
+// to help with error location, you can export/import an interface
+// and have the class implement it, if the interface changes the class
+// will error out as well as any time the class with the interface is called
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
 
   constructor() {
     this.name = faker.name.firstName();
@@ -13,5 +18,9 @@ export class User {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
